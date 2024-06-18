c7e33670531869aab4c98d26d0975719bad3b125
Daniel Alvarez <22102058@ue.edu.pe>
2024-06-18 03:00:38 -0500

    Login y dependencia de auth.

--- app/src/main/java/com/example/practica02_alvarez_martinezg/LoginActivity.kt
+++ app/src/main/java/com/example/practica02_alvarez_martinezg/LoginActivity.kt
@@ -1,10 +1,15 @@
 package com.example.practica02_alvarez_martinezg
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +20,41 @@ class LoginActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val etEmailLogin: EditText = findViewById(R.id.etEmailLogin)
+        val etPasswordLogin: EditText = findViewById(R.id.etPasswordLogin)
+        val btLogin: Button = findViewById(R.id.btLogin)
+        val btRegister: Button = findViewById(R.id.btRegister)
+        val auth =  FirebaseAuth.getInstance()
+
+        btRegister.setOnClickListener {
+            startActivity(Intent(this, RegisterUserActivity::class.java))
+        }
+
+        btLogin.setOnClickListener {
+            val email = etEmailLogin.text.toString()
+            val password = etPasswordLogin.text.toString()
+
+            auth.signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this){task->
+                    if(task.isSuccessful){
+                        Snackbar.make(
+                            findViewById(android.R.id.content)
+                            , "Ingreso satisfactorio"
+                            , Snackbar.LENGTH_LONG
+                        ).show()
+                        startActivity(
+                            Intent(this
+                            , MainActivity::class.java)
+                        )
+                    }else{
+                        Snackbar.make(
+                            findViewById(android.R.id.content)
+                            , "Credenciales inválidas"
+                            , Snackbar.LENGTH_LONG
+                        ).show()
+                    }
+                }
         }
     }
 }

0fd061112f1f60bf17a4cab9a98b53c0cb663c90
Daniel Alvarez <22102058@ue.edu.pe>
2024-06-18 04:09:11 -0500

    Diseño y model de registro .

---
+++ app/src/main/java/com/example/practica02_alvarez_martinezg/model/PlayersModel.kt
@@ -0,0 +1,11 @@
+package com.example.practica02_alvarez_martinezg.model
+
+data class PlayersModel(
+    val namePlayer: String ="",
+    val countryPlayer: String = "",
+    val typePlayer: String = "",
+    val dorsalPlayer: String = "",
+    val teamPlayer: String = "",
+    val nickPlayer: String = "",
+    val imagePlayer: String = ""
+)

7e2ae998c3899211c5821faf543e385bed143a3a
LuisM <118645322+MGLviS@users.noreply.github.com>
2024-06-18 21:52:51 -0500

    Registro Usuario

--- app/src/main/java/com/example/practica02_alvarez_martinezg/Adapter/PlayersAdapter.kt
+++ app/src/main/java/com/example/practica02_alvarez_martinezg/Adapter/PlayersAdapter.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.practica02_alvarez_martinezg.R
 import com.example.practica02_alvarez_martinezg.model.PlayersModel
@@ -38,10 +39,16 @@ class PlayersAdapter (private var lstPlayers: List<PlayersModel>):
         holder.tvCountry.text = lstPlayers[position].pcountry
         holder.tvDorsal.text = lstPlayers[position].pdorsal.toString()
         //Importar a Picasso
-        Picasso.get()
-            .load(itemPlayer.pimagen)
-            .resize(320, 350)
-            .into(holder.ivPlayer)
+        //comprobar si pimagen es null
+        if (itemPlayer.pimagen != null){
+            Picasso.get()
+                .load(itemPlayer.pimagen)
+                .resize(400, 350)
+                .into(holder.ivPlayer)
+        } else {
+            holder.ivPlayer.setImageResource(R.drawable.logo_player_final)
+            Toast.makeText(holder.itemView.context, "URL de imagen caído: ${holder.tvName}", Toast.LENGTH_LONG).show()
+        }
         holder.tvTeam.text = lstPlayers[position].pteam
         holder.tvType.text = lstPlayers[position].ptype
         holder.tvApodo.text = lstPlayers[position].papodo
--- app/src/main/java/com/example/practica02_alvarez_martinezg/RegisterUserActivity.kt
+++ app/src/main/java/com/example/practica02_alvarez_martinezg/RegisterUserActivity.kt
@@ -1,10 +1,15 @@
 package com.example.practica02_alvarez_martinezg
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterUserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +21,40 @@ class RegisterUserActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val etRmail: EditText = findViewById(R.id.etRmail)
+        val etRpass: EditText = findViewById(R.id.etRpass)
+        val btnRegisterU: Button = findViewById(R.id.btnRegisterU)
+        val btVolver: Button = findViewById(R.id.btVolver)
+
+        btVolver.setOnClickListener {
+            //Volver a LoginActivity
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+
+        //Registrar usuario, con email y password en firebase
+        btnRegisterU.setOnClickListener {
+            val email = etRmail.text.toString()
+            val password = etRpass.text.toString()
+
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Snackbar.make(
+                            findViewById(android.R.id.content)
+                            , "Usuario registrado satisfactoriamente"
+                            , Snackbar.LENGTH_SHORT
+                        ).show()
+                        startActivity(Intent(this, LoginActivity::class.java))
+                    } else {
+                        Snackbar.make(
+                            findViewById(android.R.id.content)
+                            , "Ha ocurrido un error: ${task.exception?.message}"
+                            , Snackbar.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+        }
+
     }
 }
